861db150805c073d42b0bb56c5a8ada28a556964
Alex <octanta@Alex.local>
2023-05-16 16:55:20 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {NgOptimizedImage} from "@angular/common";
+import { UserCardComponent } from './user-card/user-card.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { GridListComponent } from './grid-list/grid-list.component';
+import { PageHeadingComponent } from './page-heading/page-heading.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserCardComponent,
+    FooterComponent,
+    ContentComponent,
+    GridListComponent,
+    PageHeadingComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-heading/page-heading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-heading',
+  templateUrl: './page-heading.component.html',
+  styleUrls: ['./page-heading.component.scss']
+})
+export class PageHeadingComponent {
+
+}
