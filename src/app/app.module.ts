c853f4e7bb81856aa07f086ba73f3d168f268e02
Alex <octanta@Alex.local>
2023-05-16 23:58:43 +0300

    Add stats component to user page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import {NgOptimizedImage} from "@angular/common";
+import {NgOptimizedImage, registerLocaleData} from "@angular/common";
 import { UserCardComponent } from './user-card/user-card.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
 import { GridListComponent } from './grid-list/grid-list.component';
 import { PageHeadingComponent } from './page-heading/page-heading.component';
 import { UserPageComponent } from './user-page/user-page.component';
+import { StatsComponent } from './stats/stats.component';
+import {LOCALE_ID} from "@angular/core";
+import localeRu from '@angular/common/locales/ru';
+
+registerLocaleData(localeRu);
 
 @NgModule({
   declarations: [
@@ -21,14 +26,18 @@ import { UserPageComponent } from './user-page/user-page.component';
     ContentComponent,
     GridListComponent,
     PageHeadingComponent,
-    UserPageComponent
+    UserPageComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgOptimizedImage
   ],
-  providers: [],
+  providers: [{
+    provide:LOCALE_ID,
+    useValue:'ru'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
